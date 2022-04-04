3c921a94dd9db0416451fed1286103581954457a
iMaary <m.rodriguesnave@gmail.com>
2022-04-04 14:57:13 -0300

    some change modifiers

--- typescript-curso-1-arquivos-iniciais/app/models/negociation.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociation.ts
@@ -1,10 +1,15 @@
 export class Negociation {
 
   constructor(
-    public readonly date: Date, 
+    private readonly _date: Date, 
     public readonly quantity: number, 
     public readonly value: number
   ) {}
+
+  get date() : Date {
+    const date = new Date(this._date.getTime());
+    return date;
+  }
 
   get volume() : number {
     return this.value * this.quantity;
--- typescript-curso-1-arquivos-iniciais/dist/js/models/negociation.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/negociation.js
@@ -1,8 +1,12 @@
 export class Negociation {
-    constructor(date, quantity, value) {
-        this.date = date;
+    constructor(_date, quantity, value) {
+        this._date = _date;
         this.quantity = quantity;
         this.value = value;
+    }
+    get date() {
+        const date = new Date(this._date.getTime());
+        return date;
     }
     get volume() {
         return this.value * this.quantity;
