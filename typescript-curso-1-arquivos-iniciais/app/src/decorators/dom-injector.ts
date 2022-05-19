8976c17a1b0865493c035711ce39211778f2abcc
iMaary <m.rodriguesnave@gmail.com>
2022-05-19 15:38:11 -0300

    applying cache to dom-injector decorator copy

--- typescript-curso-1-arquivos-iniciais/app/src/decorators/dom-injector.ts
+++ typescript-curso-1-arquivos-iniciais/app/src/decorators/dom-injector.ts
@@ -3,9 +3,13 @@ export function domInjector(selector: string) {
     return function(target: any, propertyKey: string) {
         console.log(`Modifying prototype ${target.constructor.name} 
                         and adding getter to property ${[propertyKey]}`);
+        let element: HTMLElement | null = null;
         const getter = function() {
-            const element = document.querySelector(selector);
-            console.log(`searching for DOM element with ${selector} selector to inject in ${propertyKey}`);
+            if (!element) {
+                element = <HTMLElement>document.querySelector(selector);
+                console.log(`searching for DOM element with ${selector} selector to inject in ${propertyKey}`);
+
+            }
             return element;
         }
 
